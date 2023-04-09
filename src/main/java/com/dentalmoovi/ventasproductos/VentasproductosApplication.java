fa2f0bac2a10823595d4d20c771735ba3db6edb0
Oscar Gómez <98911168+Oagomezr@users.noreply.github.com>
2023-04-09 12:45:46 -0500

    first commit

---
+++ src/main/java/com/dentalmoovi/ventasproductos/VentasproductosApplication.java
@@ -0,0 +1,13 @@
+package com.dentalmoovi.ventasproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasproductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasproductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dentalmoovi/ventasproductos/VentasproductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalmoovi.ventasproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasproductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
